4fbce019f8445f2bfb9cf6ca6ca837f75034e33d
sarita110 <baralsarita110@gmail.com>
2026-03-09 23:29:16 +0545

    Update database config for Render PostgreSQL

--- config/database.php
+++ config/database.php
@@ -11,6 +11,7 @@ return [
             'prefix'                  => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        
         'mysql' => [
             'driver'         => 'mysql',
             'url'            => env('DB_URL'),
@@ -26,6 +27,22 @@ return [
             'prefix_indexes' => true,
             'strict'         => true,
             'engine'         => null,
+        ],
+
+        // 👇 WE JUST ADDED THIS POSTGRESQL BLOCK FOR RENDER 👇
+        'pgsql' => [
+            'driver'         => 'pgsql',
+            'url'            => env('DATABASE_URL'), // <-- Crucial for Render!
+            'host'           => env('DB_HOST', '127.0.0.1'),
+            'port'           => env('DB_PORT', '5432'),
+            'database'       => env('DB_DATABASE', 'forge'),
+            'username'       => env('DB_USERNAME', 'forge'),
+            'password'       => env('DB_PASSWORD', ''),
+            'charset'        => 'utf8',
+            'prefix'         => '',
+            'prefix_indexes' => true,
+            'search_path'    => 'public',
+            'sslmode'        => 'prefer',
         ],
     ],
 
